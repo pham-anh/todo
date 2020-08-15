31af0cbddef117663e94cde586d3dcec792e2dd2
Pham Quynh Anh <pqa.dev@gmail.com>
2020-08-15 22:54:59 +0900

    Make function addRun outside the var addCmd and call it inside the var addCmd.Run

--- cmd/add.go
+++ cmd/add.go
@@ -31,9 +31,11 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("add called")
-	},
+	Run: addRun,
+}
+
+func addRun(cmd *cobra.Command, args []string) {
+	fmt.Println("add called")
 }
 
 func init() {
